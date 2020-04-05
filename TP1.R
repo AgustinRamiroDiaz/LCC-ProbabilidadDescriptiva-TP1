1b962c969afc2cc459e0552be5a98c7f506fde88
Clara Hernández <57667851+claraernandez@users.noreply.github.com>
2020-04-05 16:51:15 -0300

    import

--- TP1.R
+++ TP1.R
@@ -1,10 +1,31 @@
 # Imports
-library(ggplot2)
+if (!require("RColorBrewer")) {
+  install.packages("RColorBrewer")
+  library(RColorBrewer)
+}
+
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+  library(ggplot2)
+}
+
+myPalette <- brewer.pal(5, "Set2") 
 
 # Data
 nombre <- 'base0.txt'
 tabla <- read.table(file=nombre, header=TRUE, sep='\t', fileEncoding='utf-8')
-tabla <- tabla[, c('Altura', 'DiÃ¡metro', 'InclinaciÃ³n', 'Especie', 'Origen', 'Brotes')]
-
+tabla <- tabla[, c('Altura', 'Diámetro', 'Inclinación', 'Especie', 'Origen', 'Brotes')]
+attach(tabla)
 # Plot
 plot(tabla)
+
+ggplot(tabla, aes(x=Especie, y=Altura)) + 
+  geom_bar(color="blue", fill="blue", stat = "identity") +
+  coord_flip()
+
+pie(table(Origen), border="white", col=myPalette, main= "Proporción de árboles según su origen.")
+
+ggplot(tabla, aes(x=Altura)) + geom_histogram() 
+  
+
+
