65ccbcff971e7a88e7e802c13e545a6c9a63fbbe
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2020-04-05 18:13:43 -0300

    Update TP1.R

--- TP1.R
+++ TP1.R
@@ -1,31 +1,36 @@
 # Imports
 if (!require("RColorBrewer")) {
   install.packages("RColorBrewer")
-  library(RColorBrewer)
 }
-
 if (!require("ggplot2")) {
   install.packages("ggplot2")
-  library(ggplot2)
 }
+library(RColorBrewer)
+library(ggplot2)
+library(dplyr)
 
 myPalette <- brewer.pal(5, "Set2") 
 
 # Data
 nombre <- 'base0.txt'
 tabla <- read.table(file=nombre, header=TRUE, sep='\t', fileEncoding='utf-8')
-tabla <- tabla[, c('Altura', 'Diámetro', 'Inclinación', 'Especie', 'Origen', 'Brotes')]
+tabla <- tabla[, c('Altura', 'DiÃ¡metro', 'InclinaciÃ³n', 'Especie', 'Origen', 'Brotes')]
 attach(tabla)
+
 # Plot
 plot(tabla)
+summary(tabla)
 
-ggplot(tabla, aes(x=Especie, y=Altura)) + 
+p <- tabla %>%
+  group_by(Especie) %>%
+  summarise(AlturaPromedio = mean(Altura))
+
+ggplot(p, aes(x=Especie, y=AlturaPromedio)) + 
   geom_bar(color="blue", fill="blue", stat = "identity") +
   coord_flip()
 
-pie(table(Origen), border="white", col=myPalette, main= "Proporción de árboles según su origen.")
+pie(table(Origen), border="white", col=myPalette, main= "ProporciÃ³n de Ã¡rboles segÃºn su origen.")
 
-ggplot(tabla, aes(x=Altura)) + geom_histogram() 
-  
-
+ggplot(tabla, aes(x=Altura)) + geom_histogram(binwidth = 1) 
+ggplot(tabla, aes(x=Altura)) + geom_histogram(binwidth = 5)
 
