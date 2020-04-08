86ca7af2246c331561a4418444d81556b1992d2e
Clara Hernández <57667851+claraernandez@users.noreply.github.com>
2020-04-08 18:14:23 -0300

    Empiezo a agregar colores

--- TP1.R
+++ TP1.R
@@ -35,11 +35,18 @@ attach(df)
 
 plot(df)
 summary(df)
+paleta=brewer.pal(10,name = 'Spectral')
 
 # Altura ------------------------------------------------------------------
 
-ggplot(df, aes(x = Altura)) + geom_histogram(binwidth = 1)
-ggplot(df, aes(x = Altura)) + geom_histogram(binwidth = 5)
+ggplot(df, aes(x = Altura)) + 
+  geom_histogram(color = paleta[7], fill = paleta[8],binwidth = 1) +
+  labs(x = 'Altura (m)', y = 'Cantidad de árboles',title = 'Número de árboles por altura.') #Duda por histograma
+  
+ggplot(df, aes(x = Altura)) +
+  geom_histogram(color = paleta[4], fill = paleta[5],binwidth = 5) +
+  labs(x = 'Altura (m)', y = 'Cantidad de árboles',title = 'Número de árboles por altura.') #Duda por histograma
+
 
 ggplot(
   df %>%
@@ -49,17 +56,14 @@ ggplot(
   aes(x = reorder(Especie, AlturaPromedio),
       y = AlturaPromedio)
 ) +
-  geom_col() +
-  labs(
-    x = 'Especies',
-    y = 'Altura Promedio',
-    title = 'Altura promedio según Especie',
-    subtitle = 'By Agusmonster & Clarahzz'
-  )
+  geom_col(color = paleta[1], fill = paleta[1]) +
+  labs(x = 'Especie', y = 'Altura promedio (m)',title = 'Altura promedio según la especie')
 
 # Diámetro ----------------------------------------------------------------
 
-ggplot(df, aes(x = Diámetro)) + geom_histogram(binwidth = 1)
+ggplot(df, aes(x = Diámetro)) +
+  geom_histogram(color = paleta[3], fill = paleta[4],binwidth = 1) +
+  labs(x = 'Diámetro (cm)', y = 'Cantidad de árboles',title = 'Número de árboles según el diámetro.') #Duda por histograma
 
 ggplot(
   df %>%
@@ -71,13 +75,11 @@ ggplot(
     y = DiámetroPromedio
   )
 ) +
-  geom_col() +
+  geom_col(color = paleta[2], fill = paleta[2]) +
   labs(
-    x = 'Especies',
-    y = 'Díametro Promedio',
-    title = 'Diámetro promedio según Especie',
-    subtitle = 'By Agusmonster & Clarahzz',
-    caption = 'Puto el que lee'
+    x = 'Especie',
+    y = 'Diámetro promedio (cm)',
+    title = 'Diámetro promedio según la especie'
   )
 
 ggplot(
@@ -88,13 +90,11 @@ ggplot(
   aes(x = Altura,
       y = DiámetroPromedio)
 ) +
-  geom_col() +
+  geom_col(color = "white", fill = paleta[3]) + #Color.
   labs(
-    x = 'Altura',
-    y = 'Díametro Promedio',
-    title = 'Diámetro promedio según Altura',
-    subtitle = 'By Agusmonster & Clarahzz',
-    caption = 'Puto el que lee'
+    x = 'Altura (m)',
+    y = 'Diámetro Promedio (cm)',
+    title = 'Diámetro promedio según la altura'
   )
 
 # Inclinación -------------------------------------------------------------
@@ -105,14 +105,43 @@ ggplot(
     summarise(InclinacionPromedio = mean(Inclinación)),
   aes(x = Especie, y = InclinacionPromedio)
 ) +
-  geom_bar(color = "black", fill = "pink", stat = "identity") +
-  coord_flip()
+  geom_bar(color = "blue", fill = paleta[9], stat = "identity") +
+  coord_flip() +
+  labs(x = 'Especie', y = 'Inclinación promedio (°)',title = 'Inclinación de los árboles según la especie.')
 
 # Especie -----------------------------------------------------------------
-ggplot(df %>% filter(Especie == 'Ceibo'), aes(x = Altura)) + geom_bar()
-ggplot(df %>% filter(Especie == 'Jacarandá'), aes(x = Altura)) + geom_bar()
-ggplot(df %>% filter(Especie == 'Eucalipto'), aes(x = Altura)) + geom_bar()
-ggplot(df %>% filter(Especie == 'Eucalipto'), aes(x = Altura)) + geom_bar()
+
+
+ggplot(df %>% filter(Especie == 'Acacia'), aes(x = Altura)) + 
+  geom_bar(color = paleta[3], fill = paleta[4]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Acacia por altura')
+
+ggplot(df %>% filter(Especie == 'Álamo'), aes(x = Altura)) + 
+  geom_bar(color = paleta[3], fill = paleta[4]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Álamo por altura')
+
+ggplot(df %>% filter(Especie == 'Casuarina'), aes(x = Altura)) + 
+  geom_bar(color = paleta[1], fill = paleta[2]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Casuarina por altura')
+
+ggplot(df %>% filter(Especie == 'Ceibo'), aes(x = Altura)) + 
+  geom_bar(color = paleta[1], fill = paleta[2]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Ceibo por altura')
+
+ggplot(df %>% filter(Especie == 'Eucalipto'), aes(x = Altura)) + 
+  geom_bar(color = paleta[10], fill = paleta[9]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Eucalipto por altura')
+
+ggplot(df %>% filter(Especie == 'Ficus'), aes(x = Altura)) + 
+  geom_bar(color = paleta[10], fill = paleta[9]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Ficus por altura')
+
+ggplot(df %>% filter(Especie == 'Jacarandá'), aes(x = Altura)) +
+  geom_bar(color = paleta[8], fill = paleta[7]) +
+  labs(x = 'Altura (m)', y = 'N° de árboles',title = 'Número de árboles de especie Jacarandá por altura')
+
+
+#Acá faltan árboles, los queremos todos? Duda
 
 for (e in unique(Especie)) {
   ggplot(df %>% filter(Especie == e), aes(x = Altura)) + geom_bar()
@@ -123,14 +152,15 @@ for (e in unique(Especie)) {
 
 # Origen ------------------------------------------------------------------
 
-ggplot(df %>% filter(Origen == 'Nativo/Autóctono'), aes(x = Altura)) + geom_bar()
+ggplot(df %>% filter(Origen == 'Nativo/Autóctono'), aes(x = Altura)) +
+  geom_bar(color = paleta[3], fill = paleta[4]) #Color
 
-ggplot(df %>% filter(Origen == 'Exótico'), aes(x = Altura)) + geom_bar()
+ggplot(df %>% filter(Origen == 'Exótico'), aes(x = Altura)) + geom_bar(color = paleta[1], fill = paleta[2])
 
 pie(
   table(Origen),
   border = "white",
-  col = brewer.pal(5, "Set2") ,
+  col = brewer.pal(10, "Spectral") ,
   main = "Proporción de árboles según su origen."
 )
 
@@ -144,12 +174,11 @@ ggplot(
   aes(x = Diámetro,
       y = AlturaPromedio)
 ) +
-  geom_col() +
+  geom_col(color = paleta[8], fill = paleta[8]) +
   labs(
     x = 'Diámetro',
-    y = 'Altura Promedio',
-    title = 'Altura promedio según Diámetro',
-    subtitle = 'By Agusmonster & Clarahzz'
+    y = 'Altura promedio',
+    title = 'Altura promedio según el diámetro'
   )
 
 # Inclinación promedio por especie ----------------------------------------
@@ -161,7 +190,7 @@ ggplot(
   aes(x = reorder( Especie, InclinacionPromedio),
       y = InclinacionPromedio)
 ) +
-  geom_bar(stat="identity", position=position_dodge())+
+  geom_bar(stat="identity", position=position_dodge(),color = paleta[10], fill = paleta[10])+
   labs(y = 'Inclinación promedio', x = 'Especie') +
   geom_text(aes(label=round(InclinacionPromedio, 1)), vjust=1.5, color="white", size=3.5)
 
@@ -175,7 +204,7 @@ ggplot(
     summarise(numeroBrotes = sum(Brotes)),
   aes(x = reorder(Especie, numeroBrotes), y = numeroBrotes)
 ) +
-  geom_col() +
+  geom_col(color = paleta[8], fill = paleta[8]) +
   coord_flip()
 
 ggplot(
@@ -184,7 +213,7 @@ ggplot(
     summarise(BrotesPromedio = mean(Brotes)),
   aes(x = reorder(Especie, BrotesPromedio), y = BrotesPromedio)
 ) +
-  geom_col() +
+  geom_col(color = paleta[3], fill = paleta[4]) +
   coord_flip()
 
 # Altura promedio por especie ---------------------------------------------
@@ -195,7 +224,7 @@ ggplot(
     summarise(AlturaPromedio = mean(Altura)),
   aes(x = Especie, y = AlturaPromedio)
 ) +
-  geom_col() +
+  geom_col(color = paleta[2], fill = paleta[1]) +
   coord_flip()
 
 # Tabla de frecuencia de número de brotes ---------------------------------
